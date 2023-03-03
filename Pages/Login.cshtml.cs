97022035d010ab72f2eb4173d1c448d1fdd081b6
Andy Theriault <artheriault@gmail.com>
2023-03-02 20:56:23 -0800

    Add Auth0 login/logout - Add unauthenticated categories list

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication;
+using Auth0.AspNetCore.Authentication;
+
+namespace oscars_games.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public async Task OnGet(string redirectUri)
+        {
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                .WithRedirectUri(redirectUri)
+                .Build();
+
+            await HttpContext.ChallengeAsync(
+                Auth0Constants.AuthenticationScheme,
+                authenticationProperties
+            );
+        }
+    }
+}
